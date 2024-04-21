2a2e24942c6a90b198e65af27e4cb73f86cbeb7f
Aaron Padilla <aapadill@student.hive.fi>
2024-04-21 23:24:12 +0300

    ft_memcpy.c added, haven't tested in the main yet

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -1,28 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aapadill <aapadill@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/17 14:48:55 by aapadill          #+#    #+#             */
-/*   Updated: 2024/04/21 23:20:08 by aapadill         ###   ########.fr       */
+/*   Created: 2024/04/21 19:50:47 by aapadill          #+#    #+#             */
+/*   Updated: 2024/04/21 23:22:38 by aapadill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-#define LIBFT_H
+#include "libft.h"
+void *ft_memcpy(void *restrict dst, const void *restrict src, size_t n)
+{
+	size_t i;
+	char *d;
+	const char *s;
 
-#include <stdlib.h>
-
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
-int ft_isalnum(int c);
-int ft_isascii(int c);
-int ft_isprint(int c);
-size_t ft_strlen(const char *s);
-void *ft_memset(void *b, int c, size_t len);
-void ft_bzero(void *s, size_t n);
-void *ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
-
-#endif
+	d = (char *)dst;
+	s = (const char *)src;
+	i = 0;
+	while(i < n)
+	{
+		d[i] = s[i];
+		i++;
+	}
+	return dst;
+}
