49dd83c258ca7479b6abdd053b39bbde5e1a2977
Aaron Padilla <aapadill@student.hive.fi>
2024-04-21 19:36:38 +0300

    ft_strlen now uses direct pointer arithmetic

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,11 +6,11 @@
 /*   By: aapadill <aapadill@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 10:07:46 by aapadill          #+#    #+#             */
-/*   Updated: 2024/04/21 19:25:21 by aapadill         ###   ########.fr       */
+/*   Updated: 2024/04/21 19:36:05 by aapadill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 #include "libft.h"
+
 size_t ft_strlen(const char *s)
 {
 	const char *end;
