42bb5cd7253120b91090f7bd38a95dca36ea32a2
Aaron Padilla <aapadill@student.hive.fi>
2024-04-22 10:06:41 +0300

    ft_memcpy reduced, ft_memmove in the works, just a checkpoint now

--- ft_memmove.c
+++ ft_memmove.c
@@ -1,26 +1,43 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aapadill <aapadill@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/21 19:50:47 by aapadill          #+#    #+#             */
-/*   Updated: 2024/04/22 08:34:52 by aapadill         ###   ########.fr       */
+/*   Created: 2024/04/22 08:07:06 by aapadill          #+#    #+#             */
+/*   Updated: 2024/04/22 08:58:32 by aapadill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-void *ft_memcpy(void *restrict dst, const void *restrict src, size_t n)
+
+void *ft_memmove(void *dst, const void *src, size_t len)
 {
-	char *d;
-	const char *s;
-	
-	if (dst == NULL || src == NULL)
-        return NULL;
-	d = (char *)dst;
-	s = (const char *)src;
-	while(n--)
-		*d++ = *s++;
+	char *temp_dst;
+	const char *temp_src;
+	const char *aux;
+	int overlap;
+	size_t i;
+
+	temp_dst = (char *)dst + len - 1;
+	temp_src = (const char *)src + len - 1;
+	aux = (const char *)src;
+	overlap = 0;
+	i = len;
+
+	while(i--)
+	{
+		if (aux == dst)
+			overlap = 1;
+		else
+			aux++;
+	}
+
+	if (overlap)
+		while(len--)
+			*temp_dst-- = *temp_src--;
+	else
+		ft_memcpy(dst, src, len);
 	return dst;
 }
