0d78e27e25f1e44d06c71c8d9d5737ef8fce8206
Aaron Padilla <aapadill@c1r4p1.hive.fi>
2024-04-23 19:59:27 +0300

    ft_strnstr.c almost done, just returning null might be wrong

---
+++ ft_strnstr.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aapadill <aapadill@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/23 18:16:25 by aapadill          #+#    #+#             */
+/*   Updated: 2024/04/23 19:58:22 by aapadill         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	const char	*haystack_aux;
+	const char	*needle_aux;
+	size_t	n;
+
+	if (!*needle)
+		return (char *)haystack;
+	while (*haystack && len)
+	{
+		haystack_aux = haystack;
+		needle_aux = needle;
+		n = len;
+		while (*haystack_aux == *needle_aux && *needle_aux && n)
+		{
+			haystack_aux++;
+			needle_aux++;
+			n--;
+		}
+		if (!*needle_aux)
+			return (char *)haystack;
+		haystack++;
+		len--;
+	}
+	return (0);
+}
