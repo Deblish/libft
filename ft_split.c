353bf90bacbec64376e1ba2ed91ad910fd755c9f
Aaron Padilla <aapadill@student.hive.fi>
2024-04-25 17:19:17 +0300

    split added

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: aapadill <aapadill@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/25 14:59:00 by aapadill          #+#    #+#             */
-/*   Updated: 2024/04/25 16:38:30 by aapadill         ###   ########.fr       */
+/*   Updated: 2024/04/25 17:04:34 by aapadill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,8 @@ char	**ft_split(char const *s, char c)
 	size_t	i;
 	const char	*aux;
 	char	*word_end;
-	char	word;
-	char	*words;
+	char	**words;
+	char	*found_at;
 
 	q_words = 0;
 	aux = s;
@@ -28,14 +28,17 @@ char	**ft_split(char const *s, char c)
 		aux = ft_strchr(aux, c) + 1;
 		q_words++;
 	}
-	words = malloc(q_words * sizeof(char *));
+	words = malloc((q_words + 1) * sizeof(char *));
 	aux = s;
-	
+	i = 0;
 	while (ft_strchr(aux, c))
 	{
-		word_end = ft_strchr(aux, c) - 1;
-		word = ft_stubstr(aux, word_end - aux, ft_strchr(aux, c) - aux);
-		aux = ft_strchr(aux, c) + 1;
-		words++;
+		found_at = ft_strchr(aux, c);
+		word_end = found_at - 1;
+		words[i] = ft_substr(aux, word_end - aux, found_at - aux);
+		aux = found_at + 1;
+		i++;
 	}
+	words[i] = NULL;
+	return (words);
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: aapadill <aapadill@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 14:48:55 by aapadill          #+#    #+#             */
-/*   Updated: 2024/04/25 14:46:21 by aapadill         ###   ########.fr       */
+/*   Updated: 2024/04/25 17:08:25 by aapadill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,5 +39,6 @@ char	*ft_strdup(const char *s1);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
+char	**ft_split(char const *s, char c);
 
 #endif
