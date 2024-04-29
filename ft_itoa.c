f1d18d1b8adada6a9fba22dee9600f38c0c2fe0d
Aaron Padilla <aapadill@student.hive.fi>
2024-04-29 10:15:54 +0300

    itoa almost perfect, edge cases still not checked

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: aapadill <aapadill@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/26 08:23:37 by aapadill          #+#    #+#             */
-/*   Updated: 2024/04/29 08:53:08 by aapadill         ###   ########.fr       */
+/*   Updated: 2024/04/29 10:15:02 by aapadill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,16 +43,12 @@ static int	ft_digit_counter(int j)
 char	*ft_itoa(int n)
 {
 	int		digits;
-	int		a;
-	int		b;
-	char	digit_char;
 	int		sign;
 	char	*str;
-	int		i;
 	int		j;
 
 	if (n == -2147483648)
-		return(ft_strdup("-2147483648"));
+		return (ft_strdup("-2147483648"));
 	sign = 0;
 	if (n < 0)
 	{
@@ -61,18 +57,13 @@ char	*ft_itoa(int n)
 	}
 	digits = ft_digit_counter(n);
 	str = malloc((digits + sign + 1) * sizeof(char));
-	i = digits;
 	if (sign)
 		str[0] = '-';
 	j = 0;
-	while (i--)
+	while (j < digits)
 	{
-		b = (n % (ft_exp(10, i)));
-		a = (n - b);
-		digit_char = (a / (ft_exp(10, i))) + '0';
-		//write(1, &digit_char, 1);
-		str[j + sign] = digit_char;
-		n = b;
+		str[j + sign] = (n / ft_exp(10, digits - j - 1)) % 10 + '0';
+		n %= ft_exp(10, digits - j - 1);
 		j++;
 	}
 	return (str);
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: aapadill <aapadill@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 15:08:36 by aapadill          #+#    #+#             */
-/*   Updated: 2024/04/29 08:57:49 by aapadill         ###   ########.fr       */
+/*   Updated: 2024/04/29 09:04:41 by aapadill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,7 +43,7 @@ int main (void)
 	//int i = 0;
 	//while(words[i])
 	//	printf("%s\n", words[i++]);
-	char *str = ft_itoa(-2147483648);
+	char *str = ft_itoa(-2147483647);
 	printf("%s", str);
 	return (0);
 }
