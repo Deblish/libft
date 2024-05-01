710defabcf423a4925b879800fe18b26569aca65
Aaron Padilla <aapadill@c1r5p1.hive.fi>
2024-05-01 15:27:53 +0300

    lstclear added, delone too

---
+++ ft_lstdelone_bonus.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstdelone_bonus.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aapadill <aapadill@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/01 12:15:30 by aapadill          #+#    #+#             */
+/*   Updated: 2024/05/01 12:28:20 by aapadill         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+** @description
+** Takes as a parameter a node and frees the memory of
+** the node’s content using the function ’del’ given
+** as a parameter and free the node. The memory of
+** ’next’ must not be freed.** @param
+** 
+** lst: The node to free.
+** del: The address of the function used to delete
+** the content.
+**
+** @return
+*/
+
+#include "libft.h"
+
+void	ft_lstdelone(t_list *lst, void (*del)(void*))
+{
+	if (del)
+		del(lst->content);
+	free(lst);
+}
