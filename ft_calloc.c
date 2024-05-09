f2bf5966dcad36de6f2c1731fdc979426e79af5f
Aaron Padilla <aapadill@c1r5p9.hive.fi>
2024-05-09 14:14:17 +0300

    calloc fix

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: aapadill <aapadill@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/24 13:06:00 by aapadill          #+#    #+#             */
-/*   Updated: 2024/04/29 17:42:04 by aapadill         ###   ########.fr       */
+/*   Updated: 2024/05/09 13:16:00 by aapadill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ void	*ft_calloc(size_t count, size_t size)
 {
 	void	*mem;
 
+	if (size != 0 && ((SIZE_MAX / size) <= (count * size)))
+		return (NULL);
 	mem = malloc(count * size);
 	if (!mem)
 		return (NULL);
