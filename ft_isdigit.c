91c01f6826c17fc3d16c9b8f420ba6a68aaf637d
Aaron Samir Padilla Castillo <aapadill@c1r6p12.hive.fi>
2024-07-19 17:55:22 +0300

    small fixes

--- ft_isdigit.c
+++ ft_isdigit.c
@@ -6,7 +6,7 @@
 /*   By: aapadill <aapadill@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 13:49:23 by aapadill          #+#    #+#             */
-/*   Updated: 2024/07/04 12:18:04 by aapadill         ###   ########.fr       */
+/*   Updated: 2024/07/19 17:46:12 by aapadill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,4 @@ int	ft_isdigit(int c)
 	if (c < 48 || c > 57)
 		return (0);
 	return (1);
-}
-
-int	ft_isdigit_str(char *str)
-{
-	while (*str && ft_isdigit(*str))
-		str++;
-	if (!*str)
-		return (1);
-	return (0);
 }
