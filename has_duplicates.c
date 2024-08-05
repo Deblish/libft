73918c60d2a43aa59c215d97253dcbbf5bda7904
Aaron Samir Padilla Castillo <aapadill@c3r4p3.hive.fi>
2024-08-05 14:31:03 +0300

    has_duplicates fixed, but gotta check norminette in all libft

--- has_duplicates.c
+++ has_duplicates.c
@@ -6,7 +6,7 @@
 /*   By: aapadill <aapadill@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/19 17:40:12 by aapadill          #+#    #+#             */
-/*   Updated: 2024/07/19 17:40:17 by aapadill         ###   ########.fr       */
+/*   Updated: 2024/08/05 14:28:50 by aapadill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,14 @@
 int	has_duplicates(int i, char **argv)
 {
 	int j;
-	int max;
+	//int max;
 
 	j = i - 1;
 	while (i && j)
 	{
-		max = ft_strlen(argv[i]) + ft_strlen(argv[j]);
-		if (!ft_strncmp(argv[i], argv[j], max))
+		//max = ft_strlen(argv[i]) + ft_strlen(argv[j]);
+		//if (!ft_strncmp(argv[i], argv[j], max))
+		if (ft_atoi(argv[i]) == ft_atoi(argv[j]))
 			return (1);
 		j--;
 	}
