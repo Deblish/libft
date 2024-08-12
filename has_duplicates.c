86d613c1bef28c8ffa2ac22c82a68d3a309fa63d
Aaron Padilla <aapadill@student.hive.fi>
2024-08-12 12:42:58 +0300

    fix

--- has_duplicates.c
+++ has_duplicates.c
@@ -6,7 +6,7 @@
 /*   By: aapadill <aapadill@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/19 17:40:12 by aapadill          #+#    #+#             */
-/*   Updated: 2024/08/08 14:28:11 by aapadill         ###   ########.fr       */
+/*   Updated: 2024/08/12 12:42:24 by aapadill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	has_duplicates(int i, char **argv)
 	int	j;
 
 	j = i - 1;
-	while (i && j+1)
+	while (i && j + 1)
 	{
 		if (ft_atoi(argv[i]) == ft_atoi(argv[j]))
 			return (1);
